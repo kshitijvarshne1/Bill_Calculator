ff79af2b709bbd129b77209d2de2debfc1767562
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-12 00:52:15 +0530

    add implementation for input of price and name of item

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -7,9 +7,36 @@
 
 package Calculator;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        ArrayList<ItemDetail> itemDetails= new ArrayList<>();
+        String check="y";
+        while(check.equals("y")){
+            System.out.println("Enter the item name");
+            String name= sc.nextLine();
+            System.out.println("Enter the price");
+            int price=0;
+            boolean pCheck=true;
+            while(pCheck){
+                try{
+                    price=sc.nextInt();
+                    pCheck=false;
+                }
+                catch (InputMismatchException e){
+                    System.out.println("Entered price is wrong please again input the price");
+                    sc.next();
+                }
+            }
 
+        }
+        for(ItemDetail i:itemDetails){
+            System.out.println(i);
+        }
     }
 }
 

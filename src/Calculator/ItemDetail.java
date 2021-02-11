0d1902c72aa17706afb6f803bef46c4483b03cc3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-11 21:09:23 +0530

    create Constructor,getter and setter method in ItemDetail.java

--- src/Calculator/ItemDetail.java
+++ src/Calculator/ItemDetail.java
@@ -13,5 +13,50 @@ public class ItemDetail {
     private int itemQuantity;
     private String itemType;
 
+    public ItemDetail() {
+        this.itemName = null;
+        this.itemPrice = 0;
+        this.itemQuantity = 0;
+        this.itemType = null;
+    }
+
+    public ItemDetail(String itemName, int itemPrice, int itemQuantity, String itemType) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemQuantity = itemQuantity;
+        this.itemType = itemType;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
 }
 

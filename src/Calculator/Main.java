e236a01b28bb33e4b933ebe2a8444c140a84d0e2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-12 00:52:51 +0530

    add implementation for input of quantity and type of item

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -13,30 +13,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        ArrayList<ItemDetail> itemDetails= new ArrayList<>();
-        String check="y";
-        while(check.equals("y")){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ItemDetail> itemDetails = new ArrayList<>();
+        String check = "y";
+        while (check.equals("y")) {
             System.out.println("Enter the item name");
-            String name= sc.nextLine();
+            String name = sc.nextLine();
             System.out.println("Enter the price");
-            int price=0;
-            boolean pCheck=true;
-            while(pCheck){
-                try{
-                    price=sc.nextInt();
-                    pCheck=false;
-                }
-                catch (InputMismatchException e){
+            int price = 0;
+            boolean pCheck = true;
+            while (pCheck) {
+                try {
+                    price = sc.nextInt();
+                    pCheck = false;
+                } catch (InputMismatchException e) {
                     System.out.println("Entered price is wrong please again input the price");
                     sc.next();
                 }
             }
+            System.out.println("Enter the quantity");
+            int quantity = 0;
+            boolean qCheck = true;
+            while (qCheck) {
+                try {
+                    quantity = sc.nextInt();
+                    qCheck = false;
+                } catch (InputMismatchException e) {
+                    System.out.println("Entered quantity is wrong please again input the quantity");
+                    sc.next();
+                }
+            }
+            sc.nextLine();
+            System.out.println("Enter the type of item (Raw, Manufactured and Imported )");
+            String type = sc.nextLine();
+            //itemDetails.add(new ItemDetail(name,price,quantity,type));
 
-        }
-        for(ItemDetail i:itemDetails){
-            System.out.println(i);
+            System.out.println("Do you want to enter details of any other item (y/n):");
+            check = sc.nextLine();
         }
     }
+
+    
 }
 

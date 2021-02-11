30ab21e3ebda9d8064c36c0e3050e8f88e98a146
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-11 21:05:06 +0530

    create Main.java

---
+++ src/Calculator/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Feb-21
+ *   Time: 9:04 PM
+ *   File: Main.java
+ */
+
+package Calculator;
+
+public class Main {
+}
+

f35d374f5e229746f2330740b3fb7c4d126ea360
kshitij varshney <kshitijvarshney92@gmail.com>
2021-02-11 21:03:31 +0530

    Initial commit

---
+++ src/Calculator/ItemDetail.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Feb-21
+ *   Time: 9:02 PM
+ *   File: ItemDetail.java
+ */
+
+package Calculator;
+
+public class ItemDetail {
+}
+
